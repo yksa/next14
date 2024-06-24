e2495087f9613665e0df5ffea06109f2a648ed1d
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-06-24 23:25:43 +0700

    Catch all segments 2

--- src/app/docs/[[...slug]]/page.tsx
+++ src/app/docs/[[...slug]]/page.tsx
@@ -7,13 +7,13 @@ export default function Docs({
     slug: string[];
   };
 }) {
-  if (params.slug.length === 2) {
+  if (params.slug?.length === 2) {
     return (
       <h1>
         Viewing docs for feature {params.slug[0]} and concept {params.slug[1]}
       </h1>
     );
-  } else if (params.slug.length === 1) {
+  } else if (params.slug?.length === 1) {
     return <h1>View docs for feature {params.slug[0]}</h1>;
   }
 
