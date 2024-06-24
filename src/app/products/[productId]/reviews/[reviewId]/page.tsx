c1e057fbbb3a2267877f7194863dc07f95f17898
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-06-24 23:05:48 +0700

    Nested Dynamic Routes

---
+++ src/app/products/[productId]/reviews/[reviewId]/page.tsx
@@ -0,0 +1,18 @@
+import React from "react";
+
+export default function ReviewDetail({
+  params,
+}: {
+  params: {
+    productId: string;
+    reviewId: string;
+  };
+}) {
+  return (
+    <>
+      <h1>
+        Review {params.reviewId} for product {params.productId}
+      </h1>
+    </>
+  );
+}
