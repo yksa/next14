d1e99abbf0d10764c16fb890df5eb50222c70e66
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-06-27 22:21:53 +0700

    Unmatched Routes

--- src/app/complex-dashboard/@notifications/page.tsx
+++ src/app/complex-dashboard/@notifications/page.tsx
@@ -1,5 +1,13 @@
 import Card from "@/components/card";
+import Link from "next/link";
 
 export default function Notifications() {
-  return <Card>Notifications</Card>;
+  return (
+    <Card>
+      <div>
+        <div>Notifications</div>
+        <Link href="/complex-dashboard/archived">Archived</Link>
+      </div>
+    </Card>
+  );
 }
