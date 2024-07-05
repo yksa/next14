6f70ef1bc25b41e954ab154a79a9bce4b71462de
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-07-05 14:47:14 +0700

    URL Query Parameters

--- src/app/api/comments/route.ts
+++ src/app/api/comments/route.ts
@@ -1,7 +1,15 @@
 import { comments } from "@/app/comments/data";
+import { NextRequest } from "next/server";
 
-export async function GET() {
-  return Response.json(comments);
+// http://localhost:3000/api/comments
+// http://localhost:3000/api/comments?query=first
+export async function GET(request: NextRequest) {
+  const searchParams = request.nextUrl.searchParams;
+  const query = searchParams.get("query");
+  const filteredComments = query
+    ? comments.filter((comment) => comment.text.includes(query))
+    : comments;
+  return Response.json(filteredComments);
 }
 
 export async function POST(request: Request) {
