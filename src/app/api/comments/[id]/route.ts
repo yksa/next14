63ad0b68357195560a7ad88d194579b157d079ae
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-07-05 14:23:57 +0700

    Dynamic Route Handlers

---
+++ src/app/api/comments/[id]/route.ts
@@ -0,0 +1,11 @@
+import { comments } from "@/app/comments/data";
+
+export async function GET(
+  _request: Request,
+  { params }: { params: { id: string } }
+) {
+  const comment = comments.find(
+    (comment) => comment.id === parseInt(params.id)
+  );
+  return Response.json(comment);
+}
