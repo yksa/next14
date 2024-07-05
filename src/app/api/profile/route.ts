dd0f96dca8a4f97b0ef827f8bc6e82dbcf5ade05
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-07-05 15:18:01 +0700

    Cookies in Route Handlers

--- src/app/api/profile/route.ts
+++ src/app/api/profile/route.ts
@@ -1,15 +1,22 @@
-import { headers } from "next/headers";
+import { headers, cookies } from "next/headers";
 import { NextRequest } from "next/server";
 
 export async function GET(request: NextRequest) {
   const requestHeaders = new Headers(request.headers);
   const headerList = headers();
+
+  cookies().set("resultsPerPage", "20");
+  const theme = request.cookies.get("theme");
+
   console.log("authorization header ", requestHeaders.get("authorization"));
   console.log("dkfj ", headerList.get("authorization"));
+  console.log({ theme });
+  console.log("cookie resultsPerPage ", cookies().get("resultsPerPage"));
 
   return new Response("<h1>Profile API data</h1>", {
     headers: {
       "Content-Type": "text/html",
+      "Set-Cookie": "theme=dark",
     },
   });
 }
