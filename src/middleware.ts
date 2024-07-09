e51b7e32c9f3a24b10ce5b62d5b904e371861058
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-07-09 10:18:23 +0700

    Middleware - Confitional statements

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,9 +1,13 @@
 import { NextRequest, NextResponse } from "next/server";
 
 export function middleware(request: NextRequest) {
-  return NextResponse.redirect(new URL("/", request.url));
+  //   return NextResponse.redirect(new URL("/", request.url));
+
+  if (request.nextUrl.pathname === "/profile") {
+    return NextResponse.redirect(new URL("/hello", request.url));
+  }
 }
 
-export const config = {
-  matcher: "/profile",
-};
+// export const config = {
+//   matcher: "/profile",
+// };
