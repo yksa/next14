eadc61da5ccce322ceea0ee805089484f1b426bc
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-07-09 15:09:49 +0700

    Server and Client Components

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -1,7 +1,4 @@
-export const metadata = {
-  title: "About",
-};
-
 export default function About() {
+  console.log("About server component");
   return <h1>About me</h1>;
 }
--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,5 +1,20 @@
-import React from "react";
+"use client";
+
+import React, { useState } from "react";
 
 export default function Dashboard() {
-  return <h1>Dashboard</h1>;
+  console.log("Dashboard client component");
+  const [name, setName] = useState("");
+
+  return (
+    <>
+      <h1>Dashboard</h1>
+      <input
+        type="text"
+        value={name}
+        onChange={(e) => setName(e.target.value)}
+      />
+      <p>Hello, {name}!</p>
+    </>
+  );
 }
