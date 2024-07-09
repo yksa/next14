3384ff2e531b21d5537b46ff9b567daaad342360
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-07-09 10:25:33 +0700

    Middleware - cookies and headers

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,9 +3,20 @@ import { NextRequest, NextResponse } from "next/server";
 export function middleware(request: NextRequest) {
   //   return NextResponse.redirect(new URL("/", request.url));
 
-  if (request.nextUrl.pathname === "/profile") {
-    return NextResponse.redirect(new URL("/hello", request.url));
+  //   if (request.nextUrl.pathname === "/profile") {
+  //     return NextResponse.redirect(new URL("/hello", request.url));
+  //   }
+
+  const response = NextResponse.next();
+
+  const themePreference = request.cookies.get("theme");
+  if (!themePreference) {
+    response.cookies.set("theme", "dark");
   }
+
+  response.headers.set("custom-header", "custom-value");
+
+  return response;
 }
 
 // export const config = {
