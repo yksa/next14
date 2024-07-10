999ff4909c1dce9dd4ad76febd06870c3468bd01
Ye Kyaw Swar Aung <yekyawswaraung@ayainnovation.com>
2024-07-10 22:56:11 +0700

    Loading and Error States

--- src/app/users/page.tsx
+++ src/app/users/page.tsx
@@ -7,6 +7,7 @@ type User = {
 };
 
 export default async function UsersPage() {
+  await new Promise((resolve) => setTimeout(resolve, 2000));
   const response = await fetch("https://jsonplaceholder.typicode.com/users");
   const users = await response.json();
   console.log({ users });
